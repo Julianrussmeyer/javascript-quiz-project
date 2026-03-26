5f7b9b6d173c8cd5dcf97d9a77cc8cbb6e5e0c25
Julian Russmeyer <julian@flower.ai>
2026-03-26 21:33:31 +0100

    Day 1 Progess

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,33 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
 
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    this.correctAnswers += 1;
+  }
+
+  // 6. hasEnded()
 }
